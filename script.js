8296e9c5765dac81aec472fd0166d75b258e7987
dna737 <adnanhere27@gmail.com>
2022-07-31 23:38:09 +0530

    fix the operate() function

--- script.js
+++ script.js
@@ -20,10 +20,11 @@ function divide(number1, number2){
     return number1 / number2;
 }
 
-function operator(number1, operator, number2){
-    if(operator === "+") add(number1, number2);
-    if(operator === "-") subtract(number1, number2);
-    if(operator === "*") multiply(number1, number2);
-    if(operator === "/") divide(number1, number2);
+function operate(number1, operator, number2){
+    operator = "" + operator;
+    if(operator === "+") return add(number1, number2);
+    if(operator === "-") return subtract(number1, number2);
+    if(operator === "*") return multiply(number1, number2);
+    if(operator === "/") return divide(number1, number2);
 }
 
